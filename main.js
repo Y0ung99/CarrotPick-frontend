cfc7ba87ae8c712a567218cc7f4dd30af05ab851
Y0ung99 <ancolor@knou.ac.kr>
2022-05-15 03:07:13 +0900

    함수정리, 리플레이 구현, 사운드 추가

--- main.js
+++ main.js
@@ -1,72 +1,119 @@
+'use strict';
+
 const play = document.querySelector('.play');
 const timer = document.querySelector('.timer');
 const carrotNum = document.querySelector('.num_carrot');
 const ground = document.querySelector('.ground');
 const ui = document.querySelector('.ui');
-let time = 9;
-let carrotCnt = 9;
-let clock = undefined;
-const pause = document.createElement('button')
-pause.setAttribute('class', 'play');
-pause.innerHTML = `<i class="fa-solid fa-stop"></i>`;
-ui.prepend(pause);
-pause.style.display = `none`;
+const variables = {time: null, carrotCnt: null, clock: null};
+const audios = {alert: null, bg: null, bug: null, carrot:null, win: null};
+const pause = document.createElement('button');
 
-function gameStart() {
-    createObstacle();
-    
-    pause.style.display = `block`;
-    play.style.display = `none`;
-    
-
-    pause.addEventListener('click', gameLost);
-
-    document.querySelector('.timer').innerHTML = `10`;
-    clock = setInterval(() => {
-        document.querySelector('.timer').innerHTML = `${time}`;
-        time--;
-        if (time < 0) {
-            clearInterval(clock);
-            console.log('hi');
-            document.querySelector('.timer').innerHTML = '시간초과';
-            return gameLost();
-        }
-    }, 1000);
-
-    carrotNum.innerHTML = carrotCnt;
-    ground.addEventListener('click', event => {
-        if (event.target.dataset.id === 'carrot') {
-            event.target.remove();
-            carrotNum.innerHTML = --carrotCnt;
-            clearInterval(clock);
-            if (carrotCnt === 0) return gameLost(1);
-
-        } else if (event.target.dataset.id === 'bug') {
-            event.target.remove();
-            clearInterval(clock);
-            return gameLost();
-        }
-    })
-
+function declareVariables() {
+    variables.time = 9;
+    variables.carrotCnt = 9;
+    variables.clock = undefined;
 }
 
-function gameLost(Result) {
-    !!Result ? Result = 'WIN' : Result = 'LOST';
+function declareAudios() {
+    audios.alert = new Audio('./sound/alert.wav');
+    audios.bg = new Audio('./sound/bg.mp3');
+    audios.bug = new Audio('./sound/bug_pull.mp3');
+    audios.carrot = new Audio('./sound/carrot_pull.mp3');
+    audios.win = new Audio('./sound/game_win.mp3');
+    audios.bg.autoplay = true;
+    audios.bg.loop = true;
+}
+
+function pauseBtnCreate() {
+    pause.setAttribute('class', 'play');
+    pause.innerHTML = `<i class="fa-solid fa-stop"></i>`;
+    ui.prepend(pause);
+    pause.style.display = `none`;
+}
+
+function changePlaytoPauseBtn() {
+    pauseBtnCreate();
+    pause.style.display = `block`;
+    play.style.display = `none`;
+    pause.addEventListener('click', gameResult);
+}
+
+function playTimer() {
+    timer.innerHTML = `10`;
+    variables.clock = setInterval(() => {
+        timer.innerHTML = `${variables.time}`;
+        variables.time--;
+        if (variables.time < 0) {
+            clearInterval(variables.clock);
+            timer.innerHTML = '시간초과';
+            return gameResult();
+        }
+    }, 1000);
+}
+
+function obstacleEvents() {
+    carrotNum.innerHTML = variables.carrotCnt;
+    ground.addEventListener('click', obstacleCliked);
+}
+
+function obstacleCliked(event) {
+    if (event.target.dataset.id === 'carrot') {
+        audios.carrot.play();
+        event.target.remove();
+        carrotNum.innerHTML = --variables.carrotCnt;
+        if (variables.carrotCnt === 0)  {
+            return gameResult(false);
+        }
+    } else if (event.target.dataset.id === 'bug') {
+        audios.bug.play();
+        event.target.remove();
+        return gameResult();
+    }
+}
+
+function createEndUI(Result) {
     const endUI = document.createElement('div');
     endUI.setAttribute('class', 'endUI');
     endUI.innerHTML = `
     <button class="replay"><i class="fa-solid fa-arrow-rotate-left"></i></button>
     <span>YOU ${Result}</span>
     `;
-
-    pause.removeEventListener('click', gameLost);
     ground.append(endUI);
-    
+}
+
+function gameStart() {
+    audios.alert.play();
+    audios.bg.play();
+    createObstacle(); // 당근, 벌레 생성
+    changePlaytoPauseBtn(); // 플레이 버튼 -> 퍼즈 버튼 
+    playTimer(); //  타이머 시작 이벤트
+    obstacleEvents();// 당근, 벌레 클릭 이벤트
+}
+
+function gameResult(Result = true) {
+    pause.removeEventListener('click', gameResult);
+    ground.removeEventListener('click', obstacleCliked);
+    clearInterval(variables.clock);
+    audios.bg.pause();
+    !!Result ? Result = 'LOST' : Result = 'WIN';
+    if (Result == 'WIN') audios.win.play();
+    createEndUI(Result);
 
     const replay = document.querySelector('.replay');
-    replay.addEventListener('click', (event) => {
-        history.go(0);
+    replay.addEventListener('click', () => {
+        reStart();
     });
+}
+
+function reStart() {
+    declareVariables();
+    gameStart();
+    audios.bg.load();
+}
+
+function randomXY() {
+    return `translate(${Math.floor(Math.random() * 740)}px, ${Math.floor(Math.random() * 155)}px);`;
 }
 
 function createObstacle() {
@@ -119,11 +166,8 @@ function createObstacle() {
     ;
 }
 
-function randomXY() {
-    return `translate(${Math.floor(Math.random() * 740)}px, ${Math.floor(Math.random() * 155)}px);`;
-}
-
-
 play.addEventListener('click', () => {
+    declareAudios();
+    declareVariables();
     gameStart();
 })
