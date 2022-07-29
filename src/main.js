ef7327be74f384d6db307e478783087511b8ef8f
Y0ung99 <ancolor@knou.ac.kr>
2022-07-29 18:15:40 +0900

    벌레수 변경

--- src/main.js
+++ src/main.js
@@ -4,13 +4,11 @@ import GameBuilder from './game.js';
 import Rank from './rank.js';
 
 const rankSystem = new Rank();
+rankSystem.startRankSystem();
 const game = new GameBuilder()
 .gameDuration(8)
-.carrotCount(1)
+.carrotCount(10)
 .bugCount(20)
 .build();
-
-
-rankSystem.startRankSystem();
 
 game.uiInit();
