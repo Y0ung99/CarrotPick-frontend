ea7810d777f5f1ca71c5e10edb877f3e4f2ad05a
KUM <ancolor@knou.ac.kr>
2024-07-19 00:21:45 +0900

    RANK API 엔드포인트 추가

--- src/rank.js
+++ src/rank.js
@@ -1,7 +1,7 @@
 import { configDotenv } from 'dotenv';
 configDotenv();
 
-const host = process.env.RANK_API_URL; // 랭크시스템 이용을 위해서 입력
+const host = `https://api.carrotrank.store/rank` // 랭크시스템 이용을 위해서 입력
 
 export default class Rank {
     constructor() {
